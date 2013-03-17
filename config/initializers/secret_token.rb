f034e4e8b2f6bf2d157b6225d9250891357950a5
L. John Ribar <john@nagroad.com>
2013-03-17 10:52:21 -0500

    initial app and foundation 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obo::Application.config.secret_token = '9df1a3f9c767664e098f3ee5994859e90277bf176ddedc46ccc3e583d066ea68c982853ffbb254f9914404e707d0a5218570074299d49878abbf9a11dc3abd1b'
